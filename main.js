7d323197756e87912ce3bcf7d187d9abe0246bb5
joosh119 <joshschnauber@gmail.com>
2024-01-03 22:26:07 -0500

    changed .mjs to js

--- main.js
+++ main.js
@@ -11,8 +11,8 @@
 //
 //const client = generateClient();
 
-import { generateClient } from "aws-amplify/api";
-//import { generateClient } from "./node_modules/aws-amplify/dist/esm/api/index";
+//import { generateClient } from "aws-amplify/api";
+import { generateClient } from "./node_modules/aws-amplify/dist/esm/api/index";
 import { createUser } from './src/graphql/mutations';
 import { getKorok } from './src/graphql/queries';
 
