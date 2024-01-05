f2c9099af686755b5d89164ae2754c68edd764e4
joosh119 <joshschnauber@gmail.com>
2024-01-04 23:20:46 -0500

    api.js

--- main.js
+++ main.js
@@ -14,11 +14,13 @@
 //import { generateClient } from "aws-amplify/api";
 //import { generateClient } from "./node_modules/aws-amplify/dist/esm/api/index.js";
 //import { generateClient } from "./node_modules/@aws-amplify/api/dist/esm/index.d.ts"
+//import { generateClient } from "./node_modules/@aws-amplify/api/dist/esm/API.d.ts"
+import { generateClient } from "./node_modules/@aws-amplify/api/dist/esm/API.js"
 import { createUser } from './src/graphql/mutations.js';
 import { getKorok } from './src/graphql/queries.js';
 
 
-//const client = generateClient()
+const client = generateClient()
 
 
 
