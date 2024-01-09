2e67dbeafbe1a34b2719165cc473f3f7e34fbbcd
joosh119 <joshschnauber@gmail.com>
2024-01-08 19:48:57 -0500

    fingers crossed again again

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ function getCookie(c_name){
         var c_start = document.cookie.indexOf(c_name + "=");
         if (c_start != -1) {
             c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
+            var c_end = document.cookie.indexOf(";", c_start);
             if (c_end == -1) {
                 c_end = document.cookie.length;
             }
